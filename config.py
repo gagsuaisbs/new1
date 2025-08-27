e4e08713ca6082818ab8d2ffeac15ef2934caba3
gagsuaisbs <sorsmaxshik@gmail.com>
2025-08-28 01:48:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,33 @@
 import os
+
 from telethon.sync import TelegramClient
+
 from telethon.sessions import StringSession
 
+
+
 # يجب عليك التأكد من أن هذه المتغيرات البيئية موجودة
+
 # وإلا فإن الكود لن يعمل
+
 API_ID = os.environ.get("API_ID")
+
 API_HASH = os.environ.get("API_HASH")
+
 BOT_USERNAME = os.environ.get("BOT_USERNAME")
+
 session = os.environ.get("TERMUX")
+
 token = os.environ.get("TOKEN")
 
+
+
 eighthon = TelegramClient(StringSession(session), API_ID, API_HASH)
+
 bot = TelegramClient("bot", API_ID, API_HASH).start(bot_token=token)
+
 ispay = ['yes']
+
 ispay2 = ['yes']
-# تم حذف السطر الزائد
+
+ownersaif_id = 6331807574
